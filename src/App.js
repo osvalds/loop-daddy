d7488891bc42cb62dcb1a0d06f96d0290894c88b
osvalds <osvalds.neiders@gmail.com>
2020-08-19 12:09:41 +0300

    Add some drum kits and get the sounds playing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useSound from "use-sound";
+import useHotkeys from "@reecelucas/react-use-hotkeys";
+
+
+function SoundSoundSound({name, url, keyboard}) {
+    const [play] = useSound(`${process.env.PUBLIC_URL + url}`, {interrupt: true})
+    useHotkeys(keyboard, play)
+
+
+    return <button onClick={play}>
+        {name} ({keyboard})
+    </button>
+}
+
+const sampleKit = [
+    {name: "808-bd02.wav", url: "/drums/808/808-bd02.wav"},
+    {name: "808-bd14.wav", url: "/drums/808/808-bd14.wav"},
+    {name: "808-clap2.wav", url: "/drums/808/808-clap2.wav"},
+    {name: "808-cym01.wav", url: "/drums/808/808-cym01.wav"},
+    {name: "808-hh02.wav", url: "/drums/808/808-hh02.wav"},
+    {name: "808-tme1.wav", url: "/drums/808/808-tme1.wav"}]
+
+// 1 -> q
+// 2 -> q,w
+// 3 -> q,w,e
+// 4 -> q,w,a,s
+// 5 -> q,w,e,a,s
+// 6 -> q,w,e,a,s,d
+// 7 -> q,w,e,r,a,s,d
+// 8 -> q,w,e,a,s,d,z,x
+const defaultKeyboardMap =
+    {
+        1: ["q"],
+        2: ["q", "w"],
+        3: ["q", "w", "e"],
+        4: ["q", "w", "a", "s"],
+        5: ["q", "w", "e", "a", "s"],
+        6: ["q", "w", "e", "a", "s", "d"],
+        7: ["q", "w", "e", "r", "a", "s", "d"],
+        8: ["q", "w", "e", "a", "s", "d", "z", "x"]
+    }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {sampleKit.map((sample, index) => {
+                return <SoundSoundSound key={sample.name} {...sample}
+                                        keyboard={defaultKeyboardMap[sampleKit.length][index]}/>
+            })}
+        </div>
+    );
 }
 
 export default App;
